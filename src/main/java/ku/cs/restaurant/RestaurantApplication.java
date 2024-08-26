95640ee6f60f548862d18025ca3c26ce17eff10a
Lerdphipat Kanjanarungrot <bkaosas@gmail.com>
2024-08-26 18:05:11 +0700

    add: finish project

---
+++ src/main/java/ku/cs/restaurant/RestaurantApplication.java
@@ -0,0 +1,19 @@
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 6510450917 Lerdphipat Kanjanarungroj
+ */
+
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
